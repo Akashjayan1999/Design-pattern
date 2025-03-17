2636c9e94c0a7715467c34a4395b1ec126718fb6
Akash Jayan <akash.jayan@sequantix.com>
2025-03-17 07:18:27 +0530

    Enhance Modal component with TypeScript types and composition pattern

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -7,7 +7,19 @@ import {
     DialogTrigger,
   } from "@/components/ui/dialog"
 
-const Modal = ({ children }: { children: React.ReactNode }) => {
+  type ModalProps = {
+    children: React.ReactNode;
+   
+  };
+
+  type ModalComposition = React.FC<ModalProps> & {
+    Trigger: React.FC<{ children: React.ReactNode }>;
+    Content: React.FC<{ children: React.ReactNode }>;
+    Header: React.FC<{ children: React.ReactNode }>;
+    Title: React.FC<{ children: React.ReactNode }>;
+    Description: React.FC<{ children: React.ReactNode }>;
+  }
+const Modal:ModalComposition = ({ children }) => {
   return (
     <Dialog>
     {children}
@@ -15,21 +27,21 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
-Modal.Trigger = ({ children }: { children: React.ReactNode })=>{
+Modal.Trigger = ({children})=>{
     return <DialogTrigger >{children}</DialogTrigger>
 }
-Modal.Content = ({ children }: { children: React.ReactNode }) => {
+Modal.Content = ({ children }) => {
     return <DialogContent className="sm:max-w-[425px] ">{children}</DialogContent>
   }
-Modal.Header = ({ children }: { children: React.ReactNode }) => {
+Modal.Header = ({ children }) => {
     return <DialogHeader>{children}</DialogHeader>;
   };
   
-Modal.Title = ({ children }: { children: React.ReactNode }) => {
+Modal.Title = ({ children }) => {
     return <DialogTitle>{children}</DialogTitle>;
   };
   
-Modal.Description = ({ children }: { children: React.ReactNode }) => {
+Modal.Description = ({ children }) => {
     return <DialogDescription>{children}</DialogDescription>;
   };
 
